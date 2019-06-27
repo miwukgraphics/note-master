beb48cf866a497212606059e5ca63d5defd7d410
miwukgraphics <51979895+miwukgraphics@users.noreply.github.com>
2019-06-27 12:43:59 -0700

    First

---
+++ src/components/noteForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="noteForm">
+        <div class="container">
+      <div class="row justify-content-center">
+        <div class="card col-sm-10 col-md-8 col-lg-6">
+          <h3 class="text-center">New Note</h3>
+          <div class="form-group">
+              <label> Title</label>
+              <input class="form-control" type="text" >
+          </div>    
+          <div class="form-group">
+              <label> Text</label>
+              <textarea class="form-control" > 
+              </textarea>
+          </div> 
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'noteForm',
+   
+}
+</script>
+
+<style>
+</style>
